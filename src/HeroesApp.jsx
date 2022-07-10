42aa2dfc6af87d37086e7c71f3a74ac38ee91127
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-07-10 16:25:43 -0300

    first commit

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,13 @@
+import { AppRouter } from "./router/AppRouter"
+
+export const HeroesApp = () => {
+  return (
+    <>
+        <h1>HeroesApp</h1>
+        <hr/>
+        <AppRouter />
+    </>
+  )
+}
+
+export default HeroesApp

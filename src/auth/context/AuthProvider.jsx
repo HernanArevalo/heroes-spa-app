9900328cb3ac951ed1d1a129215c005f51ed3fba
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-07-20 18:21:49 -0300

    authProvider

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -2,20 +2,50 @@ import { useReducer } from "react"
 import { AuthContext } from "./AuthContext"
 import { authReducer } from "./authReducer"
 
+import { types } from "../types/types"
 
 const initialState = {
   logged: false,
 }
 
+const init = () => {
+
+  const user = JSON.parse(localStorage.getItem('user'))
+
+  return {
+    logged: !!user,
+    user: user
+  }
+}
+
+
 export const AuthProvider = ({ children }) => {
 
     
-  const [ state, dispatch ] = useReducer( authReducer, initialState )
+  const [ authState, dispatch ] = useReducer( authReducer, initialState, init )
 
+  const login = ( name = '') => {
+
+    const action = {
+      type: types.login,
+      payload: {
+        id: '123',
+        name: name
+
+      }
+    }
+
+    dispatch( action )
+
+  }
 
 
   return (
-    <AuthContext.Provider value={ {} }>
+    <AuthContext.Provider value={ {
+      ...authState,
+      login: login,
+
+    } }>
         { children }
     </AuthContext.Provider>
   )
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,16 +1,21 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
 
     const navigate = useNavigate();
 
+    const { user } = useContext( AuthContext )
 
     const onLogout = () => {
         navigate('/login', {
             replace: true
         })
     };
+
+
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -52,7 +57,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
 
                     <span className='nav-item nav-link text-primary'>
-                        Fernando
+                        { user?.name }
                     </span>
 
                     <button

6a8689b495ab1be0ebf64afc032728053f9246af
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-07-25 18:01:20 -0300

    rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import { LoginPage } from '../auth'
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes'
+import { PrivateRoute } from './PrivateRoute'
 
 
 export const AppRouter = () => {
@@ -11,7 +12,12 @@ export const AppRouter = () => {
 
           <Route path="login" element={<LoginPage />} />
 
-          <Route path='/*' element={<HeroesRoutes />} />
+          <Route path='/*' element={
+            <PrivateRoute>
+                <HeroesRoutes />
+            </PrivateRoute>
+          } />
+
 
       </Routes>
     </>

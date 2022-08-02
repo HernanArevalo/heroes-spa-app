ae8d2c7579ab1b543d620f19e699166234b533d7
Hernan Arevalo <hernanarevalo16@gmail.com>
2022-08-02 18:00:42 -0300

    navbar test

---
+++ tests/ui/components/Navbar.test.jsx
@@ -0,0 +1,18 @@
+
+describe('Pruebas en <Navbar />', () => {
+
+    test('debe mostrar el nombre del usuario logueado', () => {
+
+
+
+
+    })
+
+    test('debe llamar el logout y navigate cuando se clickea el boton', () => {
+
+        
+
+
+    })
+
+})
